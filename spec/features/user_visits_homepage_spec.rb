cb0d41ec3d37031c41fb7b877cfb5a8affd585bc
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-11 16:19:46 -0400

    Change smoke test expectation

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'User visits homepage' do
   scenario 'successfully' do
     visit root_path
-    expect(page).to have_css 'h1', text: "Anonyo's Blog"
+    expect(page).to have_css 'h1'
   end
 end
