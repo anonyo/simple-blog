a53361d98ae81a74d5f6787da0b6f85cb498f501
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-13 14:41:23 -0400

    Visit new post path on helper instead

--- spec/support/features/post_helper.rb
+++ spec/support/features/post_helper.rb
@@ -1,5 +1,6 @@
 module Features
   def create_new_post
+    visit new_post_path
     fill_in 'Title', with: 'Ruby Rocks'
     fill_in 'Subtitle', with: 'JAVA and PHP sucks'
     fill_in 'Body', with: 'Writing Ruby is so much fun!'
