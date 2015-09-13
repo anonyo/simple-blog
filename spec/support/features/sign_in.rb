cff4f7ec02e41adb464842b284b176bbb658b95d
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-13 15:02:30 -0400

    Create admin user before signing in and user generic name and email instead of my own

--- spec/support/features/sign_in.rb
+++ spec/support/features/sign_in.rb
@@ -1,12 +1,19 @@
 module Features
   def sign_in
+    create_admin_user
     visit new_admin_session_path
-    fill_in 'Email', with: 'anonyo.hossain@gmail.com'
-    fill_in 'Password', with: 'hello123'
+    fill_in 'Email', with: 'foo@bar.com'
+    fill_in 'Password', with: 'foobar'
     click_on 'Sign in'
   end
 
   def sign_out
     click_on 'Log Out'
+  end
+  def create_admin_user
+    Admin.create(email: 'foo@bar.com',
+                 display_name: 'foo_bar',
+                 password: 'foobar',
+                 password_confirmation: 'foobar')
   end
 end
