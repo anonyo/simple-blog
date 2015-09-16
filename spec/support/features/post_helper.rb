d89b7188ea8b7c741eac199753b138acdf1d5704
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-16 15:01:50 -0400

    Targets form id instead Less chances of Capybara not finding element.

--- spec/support/features/post_helper.rb
+++ spec/support/features/post_helper.rb
@@ -1,9 +1,9 @@
 module Features
   def create_new_post
     visit new_post_path
-    fill_in 'Title', with: 'Ruby Rocks'
-    fill_in 'Subtitle', with: 'JAVA and PHP sucks'
-    fill_in 'Body', with: 'Writing Ruby is so much fun!'
+    fill_in 'post_title', with: 'Ruby Rocks'
+    fill_in 'post_subtitle', with: 'JAVA and PHP sucks'
+    fill_in 'post_body', with: 'Writing Ruby is so much fun!'
     click_on 'Submit'
   end
 end
