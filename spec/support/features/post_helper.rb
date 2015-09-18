edb51f1f253e08ab4da2a3c19d2fd4031486e8d4
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-18 12:38:54 -0400

    Adds feature helper create_about_us_page

--- spec/support/features/post_helper.rb
+++ spec/support/features/post_helper.rb
@@ -6,4 +6,11 @@ module Features
     fill_in 'post_body', with: 'Writing Ruby is so much fun!'
     click_on 'Submit'
   end
+
+  def create_about_us_page
+    visit new_page_path
+    fill_in 'page_title', with: 'About me'
+    fill_in 'page_body', with: 'I am a passionate Ruby deveoper!'
+    click_on 'Submit'
+  end
 end
