2dbe3a578c634670dd7c4e0eff4a139debe34988
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-17 21:20:36 -0400

    Tests created_at_formatted for post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
+  describe '#created_at_formatted' do
+    it 'returns date in Septemper 17, 2015' do
+      post = Post.create
+
+      expect(post.created_at_formatted).to eq expected_date
+   end
+  end
+  def expected_date
+    "#{Date.today.strftime("%B")} #{Time.now.day}, #{Time.now.year}"
+  end
 end
