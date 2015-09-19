bfed0fdef3a3e92969bf887493f76589d4657770
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-19 15:19:12 -0400

    Loads classes in lib

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ Bundler.require(*Rails.groups)
 
 module SimpleBlog
   class Application < Rails::Application
+    # Load files in lib
+    config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
