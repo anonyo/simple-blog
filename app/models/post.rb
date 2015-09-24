3fc8efc73cc39d46bb5776ba193e36b12b4887a0
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-24 19:10:53 -0400

    Refactor PostsController to inherit from BaseController

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,4 @@ class Post < ActiveRecord::Base
   def created_at_formatted
     created_at.to_s(:month_day_year)
   end
-
-  def self.find_or_build(params_id)
-    params_id ?  Post.find(params_id) : Post.new
-  end
 end
