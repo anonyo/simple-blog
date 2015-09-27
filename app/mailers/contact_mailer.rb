7c550dd4eb0ed3e75c89228edbd9f66560ac813b
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-27 11:15:50 -0400

    Changes default_from to admin_email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: admin_email
   def admin_notification(contact)
     @contact = contact
     mail(to: admin_email, subject: "You've got mail")
