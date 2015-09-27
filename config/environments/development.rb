5c59058ed5d1da34bc5c7735b0edef928a7dd765
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-27 11:08:43 -0400

    Adds letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,8 @@ Rails.application.configure do
 
   # For Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # For letter opener
+  config.action_mailer.delivery_method = :letter_opener
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
