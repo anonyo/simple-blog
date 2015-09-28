1e815653ade54e2d7996551c7bc22afc7dfe77c3
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-28 18:43:30 -0400

    Adds contact new path to nav

--- spec/features/user_can_contact_us_spec.rb
+++ spec/features/user_can_contact_us_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 feature 'User can contact us' do
   scenario 'successfully' do
-    visit new_contact_path
+    visit root_path
+    click_on 'Contact me'
     fill_in 'contact_name', with: 'John Doe'
     fill_in 'contact_email', with: 'john@mail.com'
     fill_in 'contact_message', with: "You're awesome!"
