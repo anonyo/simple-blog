f49e26743f555533ea1157a5a42da2b4371690c6
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-10-01 20:36:54 -0400

    Adds social urls to site config

--- app/models/site_config.rb
+++ app/models/site_config.rb
@@ -11,4 +11,13 @@ class SiteConfig
   def self.heading_subtitle
     ENV['SITE_HEADING_SUBTITLE']
   end
+  def self.facebook_url
+    ENV['FACEBOOK_URL']
+  end
+  def self.twitter_url
+    ENV['TWITTER_URL']
+  end
+  def self.github_url
+    ENV['GIHUB_URL']
+  end
 end
