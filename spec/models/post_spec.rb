49132bc2d53844e2977db2152ec7ae19892b3dd0
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-11-21 12:09:03 -0500

    Remove spec for created_at_formatted

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,14 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
-  describe '#created_at_formatted' do
-    it 'returns date in Septemper 17, 2015' do
-      post = Post.create
-
-      expect(post.created_at_formatted).to eq expected_date
-   end
-  end
-
   describe '#by_newest' do
     it 'orders by newest posts' do
       post_from_yesterday = Post.create(created_at: 1.day.ago)
